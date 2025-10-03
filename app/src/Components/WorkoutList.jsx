50d3ab1885144208a04179a042720e4b3d8f7e27
Trinity Saunders <trinity.saunders@hotmail.com>
2025-10-03 10:36:44 -0400

    Homepage
    Added buttons to homepage. Also centered text and changed the font size

--- app/src/Components/WorkoutList.jsx
+++ app/src/Components/WorkoutList.jsx
@@ -1,1 +1,3 @@
-// generated list of workouts based on relavice to user, muscle group or frequently used
+// generated list of workouts based on relevance to user, muscle group or frequently used
+
+//formulate the workout list that are available to the user
--- app/src/Pages/Home.jsx
+++ app/src/Pages/Home.jsx
@@ -1,9 +1,24 @@
 
 
 function App() {
+   const handleClick = (name) => {
+    alert(`You clicked ${name}!`);
+  };
   return (
-    <div> hello Home Page </div>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Home Page skeleton</h1>
+
+      <button onClick={() => handleClick("Button 1")}>Button 1</button>
+      <button onClick={() => handleClick("Button 2")}>Button 2</button>
+
+      <div style={{ marginTop: "20px" }}>
+        <a href="/about">
+          <button>Go to About Page</button>
+        </a>
+      </div>
+    </div>
   );
 }
+
 
 export default App;
